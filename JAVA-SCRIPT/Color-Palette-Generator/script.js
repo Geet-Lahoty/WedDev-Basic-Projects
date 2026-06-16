b65e37c6e47346242b337a27a1e0e08622810b9c
Geet <geetlahoty@gmail.com>
2026-06-16 15:38:25 +0530

    js tweaks

--- JAVA-SCRIPT/Color-Palette-Generator/script.js
+++ JAVA-SCRIPT/Color-Palette-Generator/script.js
@@ -2,6 +2,7 @@ const generateBtn = document.getElementById("generate-btn");
 const paletteContainer = document.querySelector(".palette-container");
 
 generateBtn.addEventListener("click", generatePalette);
+
 paletteContainer.addEventListener("click", function (e) {
   if (e.target.classList.contains("copy-btn")) {
     const hexValue = e.target.previousElementSibling.textContent;
